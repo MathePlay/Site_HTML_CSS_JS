dcffd0efb99b8098b5d32c34797d1d358f9af102
Matheus Doerzbacher <Matheus.doer@gmail.com>
2023-01-31 13:22:25 -0300

    Finalizado Modal

--- JS/script.js
+++ JS/script.js
@@ -2,9 +2,8 @@ setTimeout(ApresentarModal, 5000);
 function ApresentarModal(){
     var modal = document .querySelector(".modal");
     
-    if (modal == null){
-        alert("Não existe")
-    }else{
+    if (modal != null)
+    {
         document .querySelector(".modal").style.display = "block";
     
         document
@@ -15,4 +14,36 @@ function ApresentarModal(){
     }
 }
 
-alert(document.forms["modal_form"]);
+if (document.forms["modal_form"] != undefined){
+    // Validação
+    var form = document.forms["modal_form"];
+    
+    form.addEventListener("submit", validarFormModal);
+    form.email.addEventListener("keypress", function(){
+        form.email.className = "";
+        document.querySelector("span.nao_valido").style.display = "none"
+    })
+    
+}
+
+function validarFormModal (evt){
+    var form = document.forms["modal_form"];
+    
+    var inputEmail = form.email;
+    var valorEmail = form.email.value;
+    
+    var posicaoArroba = valorEmail.indexOf("@");
+    
+    if(
+        valorEmail != "" &&
+        valorEmail.indexOf("@") > 3 &&
+        valorEmail.lastIndexOf(".") > posicaoArroba
+    ){
+        
+    }else{
+        inputEmail.className = "nao_valido"
+        
+        document.querySelector("span.nao_valido").style.display = "block"
+        evt.preventDefault();
+    }
+}
